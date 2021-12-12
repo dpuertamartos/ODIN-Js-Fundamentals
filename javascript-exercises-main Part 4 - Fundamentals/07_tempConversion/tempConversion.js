45c431951b3d84c78a48a36fe85be58a7e964a13
David Puerta Martos <dpuerta@usal.es>
2021-12-12 02:16:21 +0100

    exercise 07 done

--- javascript-exercises-main Part 4 - Fundamentals/07_tempConversion/tempConversion.js
+++ javascript-exercises-main Part 4 - Fundamentals/07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  const formuleApplied = ((temp-32)*5)/9
+  return Math.round(formuleApplied*10)/10
 
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const formuleApplied = ((temp * 9)/5)+32
+  return Math.round(formuleApplied*10)/10
 };
 
 // Do not edit below this line
