122f74a643018e86cc2d8afeebc4b0a92a2a150f
David Puerta Martos <dpuerta@usal.es>
2021-12-12 01:43:05 +0100

    exercise 04 done

--- javascript-exercises-main Part 4 - Fundamentals/04_removeFromArray/removeFromArray.js
+++ javascript-exercises-main Part 4 - Fundamentals/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, elementsToRemove) {
+    for(let i = 1; i<arguments.length; i++){
+        array = array.filter(element => element !== arguments[i])
+    }
+    return array
 };
 
 // Do not edit below this line
