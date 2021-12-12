843335f74ab29671f1ad61d0a9faae0a85415bf5
David Puerta Martos <dpuerta@usal.es>
2021-12-12 21:22:37 +0100

    exercise 09 done

--- javascript-exercises-main Part 4 and 5- Fundamentals/09_palindromes/palindromes.js
+++ javascript-exercises-main Part 4 and 5- Fundamentals/09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const reverseString = function(str) {
+    let reversedString =""
+    for (let i=str.length-1; i>-1; i=i-1){
+        reversedString = reversedString + str[i]
+    }
+    return reversedString
+};
 
+const palindromes = function (string) {
+    const clearString = string.replace(/[&\/\\#,+()$! ~%.'":*?<>{}]/g, '');
+    return clearString.toLowerCase() === reverseString(clearString).toLowerCase()
 };
 
 // Do not edit below this line
