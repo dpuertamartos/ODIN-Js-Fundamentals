e8f0f70bbc9bd8cb8517b88247b612c4bd48ad92
David Puerta Martos <dpuerta@usal.es>
2021-12-12 21:36:03 +0100

    exercise 10  done

--- javascript-exercises-main Part 4 and 5- Fundamentals/10_fibonacci/fibonacci.js
+++ javascript-exercises-main Part 4 and 5- Fundamentals/10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n<0){return "OOPS"}
+    let previous = 0
+    let current = 1
+    for(let i=1; i<n; i++){
+        let currentstored = current
+        current=previous+current
+        previous = currentstored
+    }
+    return current
 };
 
 // Do not edit below this line
