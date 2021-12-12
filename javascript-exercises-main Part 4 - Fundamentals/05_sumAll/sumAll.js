7098cac14b93c27c6156230e19808e32fd5d5a9f
David Puerta Martos <dpuerta@usal.es>
2021-12-12 01:58:16 +0100

    exercise 05 done

--- javascript-exercises-main Part 4 - Fundamentals/05_sumAll/sumAll.js
+++ javascript-exercises-main Part 4 - Fundamentals/05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if(int1<0||int2<0||typeof int1 !== "number"||typeof int2 !== "number"){return "ERROR"}
+    let sum = 0;
+    for(let i = Math.min(...arguments); i<Math.max(...arguments)+1; i++){
+        sum = sum + i;
+    }
+    return sum
 };
 
 // Do not edit below this line
