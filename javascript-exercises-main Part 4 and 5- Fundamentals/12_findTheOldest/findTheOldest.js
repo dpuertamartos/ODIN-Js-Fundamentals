1a5cef7adbc143f8bc6a685a41d26bcc5b7c432b
David Puerta Martos <dpuerta@usal.es>
2021-12-12 21:51:07 +0100

    exercise 12 done

--- javascript-exercises-main Part 4 and 5- Fundamentals/12_findTheOldest/findTheOldest.js
+++ javascript-exercises-main Part 4 and 5- Fundamentals/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(objectsArray) {
+    const arrayAges = objectsArray.map(o=>{
+        if(!o.yearOfDeath){
+            return 2021 - o.yearOfBirth
+        }
+        else{return o.yearOfDeath - o.yearOfBirth}
+    })
+    
+    return objectsArray[arrayAges.indexOf(Math.max(...arrayAges))]
 };
 
 // Do not edit below this line
